b3b8866535d24a51620efaad431edf0fa7111afd
sbo <borawhocodess@gmail.com>
2025-09-04 18:25:56 +0200

    initialise dump downloading script

---
+++ scripts/utils.py
@@ -0,0 +1,26 @@
+# scripts/utils.py
+
+import os
+
+
+def pxp(x):
+    print()
+    print(x)
+    print()
+
+
+def get_target_dir(*subdirs, verbose=False):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(script_dir)
+
+    if verbose:
+        pxp(f"root dir: {root_dir}")
+
+    target_dir = os.path.join(root_dir, *subdirs)
+
+    os.makedirs(target_dir, exist_ok=True)
+
+    if verbose:
+        pxp(f"target dir: {target_dir}")
+
+    return target_dir
